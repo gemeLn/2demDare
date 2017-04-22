56ef612d4191c1a1628418ddf45b9e3e4c3f1827
firstdreamding <firstdreamding@gmail.com>
2017-04-22 15:33:13 -0500

    Dialong 3 33

--- src/entities/AlienCitizen.java
+++ src/entities/AlienCitizen.java
@@ -1,11 +1,15 @@
 package entities;
 
+import java.util.Random;
+
 import main.Main;
 
 public class AlienCitizen extends Entity {
 	private int GROUND = 300;
 	public int height = 100;
 	public int width = 100;
+	int randomDialog;
+	Random random = new Random();
 
 	public AlienCitizen(String link, int width, int height) {
 		super(link, width, height);
@@ -21,7 +25,25 @@ public class AlienCitizen extends Entity {
 	}
 
 	public void interact() {
-		Main.getInstance().level.dialouge("Hello Human", "Wasup");
+		randomDialog = random.nextInt(4);
+		switch(randomDialog){
+		case 0:
+			Main.getInstance().level.dialouge("Alien: Hello Human", "Me: Wasup");
+			break;
+		case 1:
+			Main.getInstance().level.dialouge("Alien: Who are you?", "Me: Human...");
+			break;
+		case 2:
+			Main.getInstance().level.dialouge("Alien: ...", "Me: ...");
+			break;
+		case 3:
+			Main.getInstance().level.dialouge("Alien: Lmao", "Me: ??");
+			break;
+		case 4:
+			Main.getInstance().level.dialouge("Alien: Pranked", "Me: Where is camera?");
+			break;
+		}
+		
 
 	}
 }
