18b2a7aca60d5bbcf03707e7e8eb4f323d6c1659
gemeLn <mtang317@gmail.com>
2017-04-22 21:34:20 -0500

    9:34
    9:34

--- src/AlienRanch/Rancher.java
+++ src/AlienRanch/Rancher.java
@@ -1,22 +1,35 @@
 package AlienRanch;
 
+import java.util.ArrayList;
+import java.util.Random;
+
+import entities.Entity;
 import graphics.Screen;
 import graphics.Texture;
 
 public class Rancher {
 	Texture bg;
+	Random r = new Random();
+	ArrayList<Entity> entities = new ArrayList<Entity>();
 
 	public Rancher() {
 		bg = new Texture("/sprites/ranch.png", 960, 540);
-		
+		for (int i = 0; i < 7; i++) {
+			entities.add(new Spider());
+		}
 	}
 
 	public void render(Screen screen) {
-		screen.drawTexture(0, 0, bg);
+		//screen.drawTexture(0, 0, bg);
+		for (Entity e : entities) {
+			e.render(screen);
+		}
 	}
 
 	public void update() {
-
+		for (Entity e : entities) {
+			e.update();
+		}
 	}
 
 }
--- src/AlienRanch/Spider.java
+++ src/AlienRanch/Spider.java
@@ -1,11 +1,51 @@
 package AlienRanch;
 
+import java.util.Random;
+
 import entities.Entity;
+import graphics.SpriteSheet;
+import graphics.Texture;
 
 public class Spider extends Entity {
+	SpriteSheet sheet;
+	int tick;
+	Random r = new Random();
 
 	public Spider() {
-		super("/sprites/spider.png", 50, 50);
+		super("/sprites/spider.png", 200, 100);
+		sheet = new SpriteSheet(sprite, 100, 100);
+		sprite = sheet.getTexture(0, 0);
+	}
+
+	public int sign() {
+		if (r.nextInt(2) == 0) {
+			return -1;
+		}
+		return 1;
+	}
+
+	public void update() {
+		tick++;
+		if (tick > 30) {
+			tick = 0;
+			xvel = sign() * (r.nextInt(7) + 1);
+			yvel = sign() * r.nextInt(7);
+		}
+
+		if (x + xvel < 0) {
+			x = 0;
+		} else if (x + width + xvel > 960) {
+			x = 960 - width;
+		} else {
+			x += xvel;
+		}
+		if (y + yvel < 0) {
+			y = 0;
+		} else if (y + height + yvel > 540) {
+			y = 540 - height;
+		} else {
+			y += yvel;
+		}
 
 	}
 
